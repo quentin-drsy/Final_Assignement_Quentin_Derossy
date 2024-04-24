61bd31dacca5f06a7d46d59146b5606db8d72017
Quentin Derossy <quentin.derossy@gmail.com>
2024-04-24 14:42:21 +0200

    Task 3 is create

---
+++ task3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(void)
+{
+    unsigned long x1 = 4234476;
+    unsigned long x2 = 8849989;
+    unsigned long y;
+    y = x1*x2;
+    printf("%ld x %ld = %ld\n", x1, x2, y);
+    return 0;
+}
